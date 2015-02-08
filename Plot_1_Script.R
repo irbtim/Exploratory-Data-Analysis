174079163c6a434561b008f60df2140fc5ef3a11
irbtim <irbtim@mail.ru>
2015-02-08 14:42:16 +0600

    Update Plot_1_Script.R

--- Plot_1_Script.R
+++ Plot_1_Script.R
@@ -1,31 +1,31 @@
 Plot 1 Script
 
-#######Load data.table library
+#Load data.table library
 library(data.table)
 data<- fread("household_power_consumption.txt")
 
-#######Clean data
+#Clean data
 class(data$Date)
 class(data$Time)
 
-#######Change the format of Date
+#Change the format of Date
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 class(data$Date)
 
-#######Subset the data
+#Subset the data
 data_subset <- data[data$Date=="2007-02-01" | data$Date=="2007-02-02"]
 class(data_subset)
 
-#######Convert data subset to a data frame
+#Convert data subset to a data frame
 data_subset <- data.frame(data_subset)
 
-#######Convert columns to numeric
+#Convert columns to numeric
 for(i in c(3:9)) {data_subset[,i] <- as.numeric(as.character(data_subset[,i]))}
 
-#######Create Date_Time variable
+#Create Date_Time variable
 data_subset$Date_Time <- paste(data_subset$Date, data_subset$Time)
 
-#######Convert Date_Time variable to proper format
+#Convert Date_Time variable to proper format
 
 data_subset$Date_Time <- strptime(data_subset$Date_Time, format="%Y-%m-%d %H:%M:%S")
 class(data_subset$Date_Time)
